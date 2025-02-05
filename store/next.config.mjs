57ead528c5af39cb12ae6054227d1e300e6407e2
Tharuka Madhushanka <79903664+Tharu119@users.noreply.github.com>
2025-02-05 13:01:31 +0530

    Update next.config.mjs

--- store/next.config.mjs
+++ store/next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+   experimental: {
+    appDir: true, // Ensure app router works
+  },
+  output: 'standalone', // Helps with Netlify deployment
   images: {
     remotePatterns: [
       {
